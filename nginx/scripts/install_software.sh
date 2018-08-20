178bcf67b2bceafbf4ae853fcbca47a4bc6d575d
sreekanth31 <kondasreekanth31@gmail.com>
2018-08-20 20:45:42 +0530

    packer with nginx script updated

---
+++ nginx/scripts/install_software.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# sleep until instance is ready
+until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+  sleep 1
+done
+
+sudo yum install wget unzip vim -y
+#install nginx
+cd /tmp
+wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+rpm -ivh epel-release-latest-7.noarch.rpm
+yum --enablerepo=epel install nginx -y
+wget https://github.com/BlackrockDigital/startbootstrap-grayscale/archive/gh-pages.zip
+unzip gh-pages.zip
+cp -r gh-pages/* /usr/share/nginx/html/
+systemctl start nginx
+
