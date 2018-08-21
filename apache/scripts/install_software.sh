f91dc6b5dae7cf7693636ad1bcd6968a8fb7ead3
sreekanth31 <40982987+sreekanth31@users.noreply.github.com>
2018-08-21 22:27:48 +0530

    Update install_software.sh

--- apache/scripts/install_software.sh
+++ apache/scripts/install_software.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-yum install wget unzip vim -y
-yum install httpd* -y
+sudo yum install wget unzip vim -y
+sudo yum install httpd* -y
 cd /tmp/
 wget https://github.com/BlackrockDigital/startbootstrap-grayscale/archive/gh-pages.zip
 unzip gh-pages.zip
 cp -r startbootstrap-grayscale-gh-pages/* /var/www/html/
 systemctl start httpd
+
 
