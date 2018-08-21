e934a0dc3ab77a99858334d4b520a0357bc504ef
sreekanth31 <40982987+sreekanth31@users.noreply.github.com>
2018-08-21 22:26:49 +0530

    Update install_software.sh

--- nginx/scripts/install_software.sh
+++ nginx/scripts/install_software.sh
@@ -5,14 +5,16 @@ until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
   sleep 1
 done
 
+#!/bin/bash
 sudo yum install wget unzip vim -y
 #install nginx
 cd /tmp
 wget http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
 rpm -ivh epel-release-latest-7.noarch.rpm
-yum --enablerepo=epel install nginx -y
+sudo yum --enablerepo=epel install nginx -y
 wget https://github.com/BlackrockDigital/startbootstrap-grayscale/archive/gh-pages.zip
 unzip gh-pages.zip
 cp -r startbootstrap-grayscale-gh-pages/* /usr/share/nginx/html/
-systemctl start nginx
+service nginx start
+
 
